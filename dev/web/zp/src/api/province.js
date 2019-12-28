8650115fe1813949248b56c0ab2fbc9b42a43a8c
unknown <506665347@qq.com>
2019-12-28 11:06:38 +0800

    '用户联系模块，黄军华写的'

---
+++ dev/web/zp/src/api/province.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: huangjh 
+ * @Date: 2019-12-25 09:40:26 
+ * @Last Modified by: huangjh
+ * @Last Modified time: 2019-12-25 10:19:04
+ */
+
+import axios from '@/utils/axios'
+/**
+ * 通过id删除省份信息
+ * @param {Object} param {id:''} 
+ */
+export function deleteProvinceById(param) {
+  return axios.post('/Province/deleteById', param);
+}
+/**
+ * 查找所有省份信息
+ */
+export function findAllProvince() {
+  return axios.get('/Province/findAll');
+}
+/**
+ * 通过id查找省份信息
+ * @param {Object} param {id:''}
+ */
+export function findProvinceById(param) {
+  return axios.get('/Province/findById', {
+    params: param
+  });
+}
+/**
+ * 保存或更新省份信息
+ * @param {Object} param
+ */
+export function saveOrUpdateProvince(param) {
+  return axios.post('/Province/saveOrUpdate', param);
+}
---
+++ dev/web/zp/src/utils/config.js
@@ -0,0 +1,43 @@
+﻿/*
+ * @Author: huangjh 
+ * @Date: 2019-12-25 11:17:02 
+ * @Last Modified by: huangjh
+ * @Last Modified time: 2019-12-26 16:19:35
+ */
+
+import Highcharts from 'highcharts';
+/* 设置后台接口基础路径 */
+let bp = "http://127.0.0.1:8899";
+Highcharts.setOptions({
+  // 设置Highcharts中的时间少8小时
+  global: { useUTC: false },
+  lang: {
+    resetZoom: '重置比例',
+    // highcharts中的暂无数据的显示
+    noData: '暂无数据'
+  }
+})
+
+function errorMsg(that,msg){
+  that.$notify.error({
+    title: '错误',
+    message: msg
+  });
+}
+//成功提示
+function successMsg(that,msg){
+  that.$notify.success({
+    title: '成功',
+    message: msg
+  });
+}
+
+//每页显示的条数
+let pageSize = 10;
+
+export default {
+  bp,
+  errorMsg,
+  successMsg,
+  pageSize,
+};
